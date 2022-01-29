8627d1a4948f1bc19c9fd1224c4bf4b1427c4dab
amitsen12 <98091018+amitsen12@users.noreply.github.com>
2022-01-29 18:59:39 +0530

    new files uploaded

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+$('menu').click(function(){
+	$(this).toggleclass('fa-times');
+$('.navbar').toggleclass('nav-toggle');
+});
+$(window).on('scroll load',function(){
+	$('#menu').removeclass('fa-times');
+	$('.navbar').removeclass('nav-toggle');
+
+	if($(window).scrolltop() >60){
+         $('header').addclass('header-active');
+     }else{
+     	$('header').removeclass('header-active')
+       }
+       $('section').each(function(){
+       	let top = $(window).scrolltop();
+       	let height =(this).height();
+       	let offset = $(this).height();
+       	if (top >=offset && top <offset + height){
+       		$('.navbar ul li a').removeclass('active');
+       		$('.navbar').find('href="#${id}"').addclass;
+       	}
+       });
+});
+})
